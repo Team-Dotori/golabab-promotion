ca8e5d2dee6e4377fd08e7e8f85691d32ed46423
Itouch-up <zxcsun1479@outlook.kr>
2021-11-17 19:51:28 +0900

    contents4

---
+++ src/Contents/Con4/Contents4.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import * as S from './Con4st'
+import note from './img/noteicon.png'
+
+const Contents4=() =>{
+  return(
+    <S.Section>
+      <S.Div>
+        <S.H>
+          어떤 메뉴가 가장 맛있었나요?
+        </S.H>
+        <S.P>
+        2주 단위로, 투표결과를 확인하실 수 있습니다.
+        </S.P>
+        <S.Note>
+          <img src={note}/>
+        </S.Note>
+        <S.Link>
+          <a href="#" style={{textDecoration:'none'}}><S.Red>투표결과 →</S.Red></a>
+        </S.Link>
+      </S.Div>
+    </S.Section>
+  )
+}
+export default Contents4
