39bc2e87bd15a794b1e768494589d8fad9a2a400
Itouch-up <zxcsun1479@outlook.kr>
2021-11-17 19:36:59 +0900

    contents2

---
+++ src/Contents/Con2/Contents2.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import * as S from './Con2st'
+
+const Contents2=() =>{
+  return(
+    <S.Section>
+      <S.Div>
+        <S.H>
+          지금은<br/>
+          <S.Pink>골라밥</S.Pink>이<br/>
+          필요한 순간
+        </S.H>
+        <S.P>
+        골라밥에게 맛있었던 메뉴를 알려주세요!<br/>
+        매달 2회, 영양사 선생님께 투표 결과를 전달해드립니다.
+        </S.P>
+      </S.Div>
+      <S.Div2>
+      <S.Aside1>
+          급식이 너무 짠 것 같아요.
+        </S.Aside1>
+        <S.Aside2>
+          음식이 덜 익어서 나와요.
+        </S.Aside2>
+        <S.Aside3>
+          반찬 양이 너무 적어요.
+        </S.Aside3>
+        <S.Aside4>
+          메뉴가 한정적이에요.
+        </S.Aside4>
+        <S.Aside5>
+          로제떡볶이 먹고 싶어요.
+        </S.Aside5>
+      </S.Div2>
+    </S.Section>
+  )
+}
+export default Contents2
