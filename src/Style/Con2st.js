1350b3afae388faeffc5452b0eb73f6949103160
Itouch-up <zxcsun1479@outlook.kr>
2021-11-22 11:23:05 +0900

    Contents2,4 병합

--- src/Style/Con2st.js
+++ src/Style/Con2st.js
@@ -1,8 +1,91 @@
 import styled from 'styled-components';
-
-export const Contents2 = styled.div`
-    background-color:black;
-    width:100%;
-    height:60vh;
-   
-`;
+export const Section=styled.section`
+  background-color: white;
+  margin: 0;
+  padding: 0;
+  width:100%;
+  height:945px;
+  display: flex;
+  align-content: space-between;
+  font-family: 'Noto Sans KR', sans-serif;
+`
+export const Div = styled.div`
+  padding-top:40vh;
+  padding-left:15vw;
+`
+export const Div2 = styled.div`
+  padding-top:25vh;
+  padding-left:15vw;
+  font-size: 20px;
+  font-weight: bold;
+  font-style: normal;
+  text-align: center;
+`
+export const Pink = styled.a`
+  color:#ffacac;
+`
+export const P = styled.p`
+  color:#AE9696;
+  padding-top: 30px;
+  font-size:24px;
+`
+export const H = styled.p`
+  font-size: 64px;
+  font-weight: bold;
+`
+export const Aside1 = styled.aside`
+  margin-bottom: 40px;
+  width:315px;
+  height:71px;
+  border-radius: 10px;
+  padding: 18px 32px;
+  background-color: #FFEAEA;
+  color: #663E3E;
+  flex-direction: row;
+  margin-left: 63px;
+`
+export const Aside2 = styled.aside`
+  margin-bottom: 40px;
+  width:309px;
+  height:71px;
+  border-radius: 10px;
+  padding: 18px 32px;
+  background-color: #FFEAEA;
+  color: #663E3E;
+  opacity: 0.9;
+  flex-direction: row;
+`
+export const Aside3 = styled.aside`
+  margin-bottom: 40px;
+  width:287px;
+  height:71px;
+  border-radius: 10px;
+  padding: 18px 32px;
+  background-color: #FFEAEA;
+  color: #663E3E;
+  opacity: 0.8;
+  flex-direction: row;
+  margin-left:91px;
+`
+export const Aside4 = styled.aside`
+  margin-bottom: 40px;
+  width:276px;
+  height:71px;
+  border-radius: 10px;
+  padding: 18px 32px;
+  background-color: #FFEAEA;
+  color: #663E3E;
+  opacity: 0.7;
+  flex-direction: row;
+`
+export const Aside5 = styled.aside`
+  width:304px;
+  height:71px;
+  border-radius: 10px;
+  padding: 18px 32px;
+  background-color: #FFEAEA;
+  color: #663E3E;
+  opacity: 0.6;
+  flex-direction: row;
+  margin-left: 74px;
+`
--- src/Style/Con4st.js
+++ src/Style/Con4st.js
@@ -1,8 +1,41 @@
 import styled from 'styled-components';
-
-export const Contents4 = styled.div`
-    background-color:skyblue;
-    width:100%;
-    height:70vh;
-    
-`;
+export const Section=styled.section`
+  background-color: #FFEAEA;
+  margin: 0;
+  padding: 0;
+  width:100%;
+  height:945px;
+  display: flex;
+  align-content: space-between;
+  font-family: 'Noto Sans KR', sans-serif;
+`
+export const Div = styled.div`
+  padding-top:15vh;
+  text-align: center;
+`
+export const Div2 = styled.div`
+`
+export const P = styled.p`
+  color:#AE9696;
+  padding-top: 30px;
+  font-size:24px;
+  width: 100vw;
+`
+export const H = styled.p`
+  font-size: 64px;
+  font-weight: bold;
+`
+export const Note = styled.div`
+  margin-top: 32px;
+  position: relative;
+  left: 43vw;
+  width: 278px;
+`
+export const Red = styled.a`
+  color: #FF5151;
+`
+export const Link = styled.div`
+  font-size: 30px;
+  margin-top: 40px;
+  font-weight: bold;
+`
