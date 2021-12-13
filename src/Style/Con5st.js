eabf9521cbf9c7b566348851bf00376a03131b5c
iseolin76 <iseolin76@gmail.com>
2021-12-14 01:25:56 +0900

    [UPDATE] Modify the layout for the middle alignment

--- src/Style/Con5st.js
+++ src/Style/Con5st.js
@@ -1,49 +1,50 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Contents5 = styled.div`
-    background-color:#FFACAC;
-    width:100%;
-    height:70vh;
-    display:flex;
-    justify-content:center;
+  background-color: #ffacac;
+  width: 100%;
+  height: 70vh;
+  display: flex;
+  justify-content: center;
 `;
 
 export const Content = styled.div`
-    display:flex;
-    flex-direction:column;
-    justify-content:space-evenly;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
 `;
 
 export const Ment1 = styled.div`
-    font-size:3.6rem;
-    font-weight:bold;
-    color:#FFFFFF;
-    text-align:center;
-    padding-top:20vh;
-    font-family:"Noto Sans";
+  font-size: 3.6rem;
+  font-weight: bold;
+  color: #ffffff;
+  text-align: center;
+  padding-top: 20vh;
+  font-family: "Noto Sans";
 `;
 
 export const Ment2 = styled.div`
-    font-size:1.4rem;
-    font-weight:400;
-    color:#FFFFFF;
-    text-align:center;
-    padding-bottom:5vh;
-    font-family:"Noto Sans KR";
+  font-size: 1.4rem;
+  font-weight: 400;
+  color: #ffffff;
+  text-align: center;
+  padding-bottom: 5vh;
+  font-family: "Noto Sans KR";
 `;
 
 export const Votebutton = styled.button`
-    width:7vw;
-    height:5.2vh;
-    font-size:1.2rem;
-    font-weight:bold;
-    font-family:"Noto Sans KR";
-    border-radius:5px;
-    border:none;
-    color:#663E3E;
-    background-color:#FFEAEA;
-    margin:0 auto 15vh;
-    :hover{
-        cursor: pointer;
-    }
+  width: 7vw;
+  height: 5.2vh;
+  font-size: 1.2rem;
+  font-weight: bold;
+  font-family: "Noto Sans KR";
+  border-radius: 5px;
+  border: none;
+  color: #663e3e;
+  background-color: #ffeaea;
+  margin: 0 auto 15vh;
+  :hover {
+    cursor: pointer;
+  }
 `;
