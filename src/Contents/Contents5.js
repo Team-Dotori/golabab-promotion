c1f2c5113404c3a0df170f6165a506501d8492c4
iseolin76 <iseolin76@gmail.com>
2021-12-14 01:24:47 +0900

    [UPDATE] change it to text that it was launched

--- src/Contents/Contents5.js
+++ src/Contents/Contents5.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import * as S from '../Style/Con5st';
+import React from "react";
+import * as S from "../Style/Con5st";
 
 //react route 사용함 ㅋㅋ
 const Contents5 = () => {
-    return (
-        <>
-            <S.Contents5>
-                <S.Content>
-                    <S.Ment1>
-                        2021년 11월 런칭예정
-                    </S.Ment1>
-                    <S.Ment2>
-                        골라밥이 여러분께 찾아가기 까지 기다려주세요!
-                    </S.Ment2>
-                    <S.Votebutton type="button" onClick={()=>window.location.href='https://discord.gg/Wt65TuAMpe'}>
-                        체험하기
-                    </S.Votebutton>
-                </S.Content>
-            </S.Contents5>   
-        </>
-    );
+  return (
+    <>
+      <S.Contents5>
+        <S.Content>
+          <S.Ment1>2021년 12월 골라밥 런칭완료</S.Ment1>
+          <S.Ment2>골라밥이 여러분께 도착했어요!</S.Ment2>
+          <a href={process.env.REACT_APP_DEV_SERVER}>
+            <S.Votebutton>개발서버</S.Votebutton>
+          </a>
+        </S.Content>
+      </S.Contents5>
+    </>
+  );
 };
 
 export default Contents5;
