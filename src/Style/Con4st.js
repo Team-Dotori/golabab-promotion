3b57d85998a55bb400f5a69f5574d12c57049d96
iseolin76 <iseolin76@gmail.com>
2021-12-14 08:47:17 +0900

    [FIX] 가로 스크롤이 생기는 현상

--- src/Style/Con4st.js
+++ src/Style/Con4st.js
@@ -1,44 +1,40 @@
-import styled from 'styled-components';
-export const Section=styled.section`
-  background-color: #FFEAEA;
+import styled from "styled-components";
+export const Section = styled.section`
+  background-color: #ffeaea;
   margin: 0;
   padding: 0;
-  width:100%;
-  height:945px;
+  width: 100%;
+  height: 945px;
   display: flex;
+  justify-content: center;
   align-content: space-between;
-  font-family: 'Noto Sans KR', sans-serif;
-`
+  font-family: "Noto Sans KR", sans-serif;
+`;
 export const Div = styled.div`
-  padding-top:15vh;
+  padding-top: 15vh;
   text-align: center;
-`
-export const Div2 = styled.div`
-`
+`;
+export const Div2 = styled.div``;
 export const P = styled.p`
-  color:#AE9696;
+  color: #ae9696;
   padding-top: 30px;
-  font-size:1.4rem;
-  width: 100vw;
-  font-family:"Noto Sans KR";
-`
+  font-size: 1.4rem;
+  font-family: "Noto Sans KR";
+`;
 export const H = styled.p`
   font-size: 3.6rem;
   font-weight: bold;
-  font-family:"Noto Sans";
-`
+  font-family: "Noto Sans";
+`;
 export const Note = styled.div`
   margin-top: 32px;
-  position: relative;
-  left: 43vw;
-  width: 278px;
-`
+`;
 export const Red = styled.a`
-  color: #FF5151;
-  font-family:"Noto Sans KR";
-`
+  color: #ff5151;
+  font-family: "Noto Sans KR";
+`;
 export const Link = styled.div`
   font-size: 30px;
   margin-top: 40px;
   font-weight: bold;
-`
+`;
