eb5d81c56a0d7684150459814ab206b1a62cad5f
Itouch-up <zxcsun1479@outlook.kr>
2021-12-14 10:53:30 +0900

    Fixed Cont2

--- src/Style/Con2st.js
+++ src/Style/Con2st.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 export const Section=styled.section`
   background-color: white;
     width:100%;
-    height:60vh;
+    height:70vh;
     display:flex;
     justify-content:center;
 `
