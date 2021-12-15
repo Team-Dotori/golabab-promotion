b7053423d6f2ed621dc428b4388e9f47b14875d1
iseolin76 <iseolin76@gmail.com>
2021-12-15 20:56:21 +0900

    [UPDATE] font style 및 a 태그를 위한 Global Style 추가

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import './App.css';
-import {createGlobalStyle} from 'styled-components';
-import MainPage from './Main';
+import { createGlobalStyle } from "styled-components";
+import MainPage from "./Main";
 
 const GlobalStyle = createGlobalStyle`
   *{
     margin: 0;
     padding: 0;
     box-sizing:border-box;
+    font-family: "Noto Sans KR",serif;
+  }
+
+  a {
+    text-decoration: none;
   }
 `;
-
 
 function App() {
   return (
     <>
-     <GlobalStyle />
-     <MainPage/>
+      <GlobalStyle />
+      <MainPage />
     </>
   );
 }
