ad838fe5e7e53bc7e91de68249b183c853c26b12
iseolin76 <iseolin76@gmail.com>
2021-12-16 12:06:04 +0900

    [UPDATE] Add a code description comment

--- src/Style/Arrange.js
+++ src/Style/Arrange.js
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 import device from "../config/reactive";
 
+/**
+ * 1개의 자식 요소를 가운데 정렬하기 위해 사용할 수 있습니다.
+ *
+ * @param {string} height: Section height, default: 60vh
+ * @param {string} backgroundColor: Section background color
+ */
 const ArrangeMiddleSection = styled.section`
   width: 100%;
   height: ${(p) => p.height || "auto"};
@@ -11,6 +17,9 @@ const ArrangeMiddleSection = styled.section`
   padding: 100px 0;
 `;
 
+/**
+ * 2개의 자식 요소를 가운데 정렬하기 위해 사용할 수 있습니다.
+ */
 export const ArrangeTwoElement = styled.div`
   width: 60vw;
   display: flex;
