436153fa238dc78ddce6098cf959e350cfea19bf
iseolin76 <iseolin76@gmail.com>
2021-12-16 12:04:14 +0900

    [UPDATE] Apply LinkButton Component

--- src/Contents/Contents5.js
+++ src/Contents/Contents5.js
@@ -1,5 +1,6 @@
 import React from "react";
 import * as S from "../Style/Con5st";
+import { LinkButton } from "../Components/Button";
 import ArrangeMiddleSection from "../Style/Arrange";
 
 //react route 사용함 ㅋㅋ
@@ -10,9 +11,14 @@ const Contents5 = () => {
         <S.Content>
           <S.Ment1>2021년 12월 골라밥 런칭완료</S.Ment1>
           <S.Ment2>골라밥이 여러분께 도착했어요!</S.Ment2>
-          <a href={process.env.REACT_APP_DEV_SERVER}>
-            <S.Votebutton>개발서버</S.Votebutton>
-          </a>
+          <S.Votebutton>
+            <LinkButton
+              buttonColor="#ffeaea"
+              fontColor="#663e3e"
+              name="개발서버"
+              link={process.env.REACT_APP_DEV_SERVER}
+            />
+          </S.Votebutton>
         </S.Content>
       </ArrangeMiddleSection>
     </>
